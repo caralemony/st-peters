b6bcb0a7a69eac0c975ba6adcd94644b43edb343
caralemony <carolinelappleby@gmail.com>
2017-12-18 16:40:02 +0000

    home page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const routes = require('./routes');
+// const routes = require('./routes');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(path.join(__dirname, '..', 'public')));
-// app.use(routes);
+
 
 module.exports = app;
--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,8 @@ const router = express.Router();
 const home = require('./index');
 
 router.get('/', home.get);
+
+router.get('/newsCall', (req, res) => {
+  const url = req.url;
+  console.log(url);
+})
