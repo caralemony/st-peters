00a2c4ae50ac57f29864742ff87375ac3d529d9d
caralemony <carolinelappleby@gmail.com>
2017-12-19 18:31:09 +0000

    newsApi url and request done

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,24 @@
-require('routes');
-require('env')('env2');
+const path = require('path');
+require('env2')('./config.env');
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+require('./routes');
+const NEWS_API = process.env.NEWS_API;
+
 
 const newsAPI = (searchTerm) => {
+const xhr = new XMLHttpRequest();
+const url =`https://newsapi.org/v2/everything?q=${searchTerm}&sources=daily-mail&apiKey=${NEWS_API}`;
+console.log(url);
 
-const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
+  xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("results").innerText = this.responseText;
+    const bodyRes = JSON.parse(this.responseText);
+    const articleUrl = bodyRes.articles[0].url);
+    watsonCall(articleUrl);
     }
   };
-  xhttp.open("GET", `https://newsapi.org/v2/everything?q=${searchTerm}&sources=daily-mail&apiKey=${NEWS_API}`, true);
-  xhttp.send();
-}
-}
+  xhr.open("GET", url, true);
+  xhr.send();
+};
+
+module.exports = newsAPI;
