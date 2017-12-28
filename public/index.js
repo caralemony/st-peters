c198ca57eb9668410b27a0eff4d7f4ab3c191153
caralemony <carolinelappleby@gmail.com>
2017-12-28 18:33:24 +0000

    creating new branch

--- public/index.js
+++ public/index.js
@@ -1,1 +1,10 @@
 var results = document.getElementById('results');
+
+
+xhr.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+
+};
+xhr.open("GET", '/newsCall', true);
+xhr.send();
+};
